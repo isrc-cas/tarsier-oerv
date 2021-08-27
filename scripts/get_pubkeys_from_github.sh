1c611a074a3000f6987ea832f617d1cf80881f3f
Wei Wu <wuwei2016@iscas.ac.cn>
2021-08-27 19:02:06 +0800

    添加一个小脚本用于同学们学习

---
+++ scripts/get_pubkeys_from_github.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 警告：这是一个临时的脚本，仅用于内部使用和提供给实习生学习
+#       这个脚本的正确性依赖于 members.md 的格式。
+
+die () {
+	echo "$*"
+	exit 3
+}
+
+LIST='members.md'
+[ -f "$LIST" ] || LIST='../members.md'
+[ -f "$LIST" ] || die "No members.md found"
+
+[ -d 'pubkeys' ] || mkdir pubkeys || die "pubkeys/ creation failed"
+
+cat "$LIST" | grep '|' | tail -n +3 | cut -f6 -d'|' | \
+while read s; do
+	echo "====$s===="
+	pp curl https://github.com/${s}.keys > pubkeys/pubkeys.${s}
+done
+
+echo "Have a nice day."
