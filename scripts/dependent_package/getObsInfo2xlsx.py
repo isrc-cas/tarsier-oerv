341dfd5a548bbd952bbe8866b568e3b9c6820561
xijing <84754181+xijing21@users.noreply.github.com>
2021-09-22 11:40:52 +0800

    update

--- scripts/dependent_package/getObsInfo2xlsx.py
+++ scripts/dependent_package/getObsInfo2xlsx.py
@@ -10,10 +10,7 @@ from openpyxl.styles import Side, Border
 import time
 import datetime
 
-unresolvable_url = 'https://build.openeuler.org/project/monitor/openEuler:Mainline:RISC-V?arch_riscv64=1&defaults=0&repo_standard_riscv64=1&unresolvable=1'
-succeeded_url = 'https://build.openeuler.org/project/monitor/openEuler:Mainline:RISC-V?arch_riscv64=1&defaults=0&repo_standard_riscv64=1&succeeded=1'
-failed_url = 'https://build.openeuler.org/project/monitor/openEuler:Mainline:RISC-V?arch_riscv64=1&defaults=0&failed=1&repo_standard_riscv64=1'
-
+base_url = 'https://build.openeuler.org/project/monitor/openEuler:Mainline:RISC-V?arch_riscv64=1&defaults=0&repo_standard_riscv64=1'
 
 def get_packagelist(url):
     response = requests.get(url)
@@ -22,71 +19,78 @@ def get_packagelist(url):
     pacakgedatalist = [x for x in datalist if x.startswith('<tbody')]
     packagedata = pacakgedatalist[0].split("='")
     data_packagenames = packagedata[1].split("]'")[0].replace("[","").replace("&quot;","").split(", ")
-    # tmp_data_statushash = packagedata[3].replace("&quot;","").replace("{standard_riscv64:{riscv64:","").replace("}}' data-tableinfo","")
 
-    # packagelistdata = packagedata[1].replace('<tbody data-packagenames', '')
-    # packagelistdata = packagelistdata.replace(' data-project', '')
-    # packagelistdata = re.sub(r'&quot;', '', packagelistdata)
-    # packagelistdata = re.sub(r' ', '', packagelistdata)
-    # packagelist = packagelistdata[2:-2].split(',')
-    # print (len(packagelist))
     return data_packagenames
 
 # 定义状态对应的编号（用于excle数据分析和sheetindex）
 def getCode(status):
-    code = 3
+    code = 6
     if status == 'succeeded':
         code = 0
-    if status == 'unresolvable':
-        code = 1
     if status == 'failed':
+        code = 1
+    if status == 'unresolvable':
         code = 2
+    if status == 'broken':
+        code = 3
+    if status == 'disabled':
+        code = 4
+    if status == 'excluded':
+        code = 5
 
     return code
 
-def writeToExcle(filepath, datelist, title):
-    status = getCode(title)
-
+def writeToExcle(filepath, datelist, dt, title):
     wb = load_workbook(filepath)
-    # sheetindex = len(wb.worksheets)
-    ws = wb.worksheets[0]
-    for itPkgname in datelist:
-        ws.append([itPkgname, status, title])
 
-    print(title + "  is done! ",len(datelist)," datas write into the file ：" + filepath)
+    # 记录统计数据
+    wst = wb.worksheets[0]
+    wst.append([dt, title, len(datelist)])
+
+    # 记录详细数据
+    ws = wb[dt]
+    for itPkgname in datelist:
+        ws.append([itPkgname, getCode(title), title])
+
+    print(title + ": ",len(datelist))
     wb.save(filepath)
 
 
-if __name__=="__main__":
-    # 获取各状态下的packagesname
-    unresolvable_packagelist = get_packagelist(unresolvable_url)
-    succeeded_packagelist = get_packagelist(succeeded_url)
-    failed_packagelist = get_packagelist(failed_url)
 
+if __name__=="__main__":
     # 根据日期新建excle，并写入数据。用于归档和数据分析用
     nowtime = time.strftime("%Y%m%d-%H%M", time.localtime())
     print(nowtime)
-    sheetName = 'obsBuild-' + nowtime
+    sheetName = 'obsBuild'
     obsdatafolder = os.getcwd().replace("scripts\dependent_package",'data\obsBuildStatus')
     obsfilepath = os.path.join(obsdatafolder, sheetName+'.xlsx')
 
-    wb = Workbook()
-    # ws = wb.create_sheet("obs-status", 0)
-    # ws.column_dimensions["A"].width = 20
-    # ws.column_dimensions["B"].width = 20
-    # ws.append(['name', 'code'])
-    # ws.append(['succeeded', 0])
-    # ws.append(['unresolvable', 1])
-    # ws.append(['failed', 2])
+    try:
+        wb = load_workbook(obsfilepath)
+        ws = wb['buildresultsTotal']
+    except FileNotFoundError:
+        wb = Workbook()
+        wst = wb.create_sheet("buildresultsTotal", 0)  #记录统计信息
+        wst.append(['datetime','statusname','totalnum'])
+        wst.column_dimensions["A"].width = 20
+        wst.column_dimensions["B"].width = 20
+        wst.column_dimensions["C"].width = 20
 
-    ws = wb.create_sheet("buildresults", 0)
+    sheetindex = len(wb.worksheets)
+    ws = wb.create_sheet(nowtime, sheetindex)  #按日期记录详细构建信息
     ws.append(['packagename', 'statuscode', 'statusname'])
     ws.column_dimensions["A"].width = 20
     ws.column_dimensions["B"].width = 20
     ws.column_dimensions["C"].width = 20
+
     wb.save(obsfilepath)
 
-    # 写入数据到excle
-    writeToExcle(obsfilepath, succeeded_packagelist, "succeeded")
-    writeToExcle(obsfilepath, unresolvable_packagelist, "unresolvable")
-    writeToExcle(obsfilepath, failed_packagelist, "failed")
+    # # 获取各状态下的packagesname并写入数据到excle
+    queryDate = ["succeeded","failed","unresolvable","broken","disabled","excluded"]
+    for qd in queryDate:
+        url = base_url+"&"+qd+"=1"
+        # print(qd,"url=",url)
+        packagelist = get_packagelist(url)
+        writeToExcle(obsfilepath, packagelist, nowtime, qd)
+
+    print('All datas in file ：', obsfilepath)
