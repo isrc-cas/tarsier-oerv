d23864b94cc0803b0baa489fa3a1b008aca186ec
jean9823 <jean9823@163.com>
2023-06-25 19:43:04 +0800

    add package repository

--- scripts/obs_pkg_rebuild/parameters.py
+++ scripts/obs_pkg_rebuild/parameters.py
@@ -1,4 +1,5 @@
 obs_account = {'username':'username of your obs account', 'password':'password of your obs account'}
 obs_url = 'https://build.tarsier-infra.com'
 obs_project = 'openEuler:23.03'
+obs_buildinfo= {'repo': '23.03', 'arch': 'riscv64'}
 obs_packagelist = ['adcli', 'python-docker']
